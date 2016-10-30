7034aa22e04b34249aed8c606903fc70f5969229
Kevinpkendall <kevinkendall@ymail.com>
2016-10-29 20:46:18 -0500

    finish Flicklist 4 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,13 +8,13 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "8e888fa39ec243e662e1fb738c42ae99", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
     var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
-    return baseImageUrl + movie.poster_path; 
+    return baseImageUrl + movie.poster_path;
   }
 }
 
@@ -26,11 +26,11 @@ var api = {
  * the callback function that was passed in
  */
 
-// TODO 1
+// TODO 1 (DONE)
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
+function discoverMovies(callback, keywords) {
 
-  // TODO 2 
+  // TODO 2 (DONE)
   // ask the API for movies related to the keywords that were passed in above
   // HINT: add another key/value pair to the `data` argument below
 
@@ -38,9 +38,11 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords
     },
     success: function(response) {
       model.browseItems = response.results;
+      console.log(response.results);
       callback(response);
     }
   });
@@ -48,58 +50,58 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/keywords endpoint of the API, using the 
+ * Makes an AJAX request to the /search/keywords endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, invokes the supplied callback function, passing in
  * the API's response.
  */
 function searchMovies(query, callback) {
-  // TODO 3
+  // TODO 3 (DONE)
   // change the url so that we search for keywords, not movies
-
-
-  // TODO 4
-  // when the response comes back, do all the tasks below:
-
-
-  // TODO 4a
-  // create a new variable called keywordIDs whose value is an array of all the
-  // `.id` values of each of the objects inside reponse.results
-  // HINT use the array map function to map over response.results
-
-
-  // TODO 4b
-  // create a new variable called keywordsString by converting 
-  // the array of ids to a comma-separated string, e.g.
-  //      "192305,210090,210092,210093"
-  // HINT: use the Array join function
-
-
-  // TODO 4c
-  // instead of a comma-separated string, we want the ids
-  // to be spearated with the pipe "|" character, eg:
-  //     "192305|210090|210092|210093"
-  // HINT: pass an argument to the join function
-
-
-  // TODO 4d
-  // when the response comes back, call discoverMovies, 
-  // passing along 2 arguments:
-  // 1) the callback 
-  // 2) the string of keywords
-
-
   $.ajax({
-    url: api.root + "/search/movie",
+    url: api.root + "/search/keyword",
     data: {
       api_key: api.token,
       query: query
     },
     success: function(response) {
-      console.log(response);
+      console.log(response + "line 69");
+      // TODO 4 (DONE)
+      // when the response comes back, do all the tasks below:
+      // TODO 4a (DONE)
+      // create a new variable called keywordIDs whose value is an array of all the
+      // `.id` values of each of the objects inside response.results
+      // HINT use the array map function to map over response.results
+      var keywordIDs = response.results.map(function getID (x) {
+        return x.id;
+      });
+      console.log("---keywordIDs---" + keywordIDs);
+      // TODO 4b (DONE)
+      // create a new variable called keywordsString by converting
+      // the array of ids to a comma-separated string, e.g.
+      //      "192305,210090,210092,210093"
+      // HINT: use the Array join function
+      // TODO 4c (DONE)
+      // instead of a comma-separated string, we want the ids
+      // to be separated with the pipe "|" character, eg:
+      //     "192305|210090|210092|210093"
+      // HINT: pass an argument to the join function
+      var keywordsString = keywordIDs.join("|");
+      console.log("---keywordsString---" + keywordsString);
+      // TODO 4d (DONE)
+      // when the response comes back, call discoverMovies,
+      // passing along 2 arguments:
+      // 1) the callback
+      // 2) the string of keywords
+      discoverMovies(callback,keywordsString);
     }
   });
+
+  // function getID (x) {
+  //   return x.id;
+  // }
+
 }
 
 
@@ -115,36 +117,36 @@ function render() {
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-      
+
     // movie poster
     var poster = $("<img></img>")
-      .attr("src", api.posterUrl(movie))
-      .attr("class", "img-responsive");
+        .attr("src", api.posterUrl(movie))
+        .attr("class", "img-responsive");
 
     // "I watched it" button
     var button = $("<button></button>")
-      .text("I watched it")
-      .attr("class", "btn btn-danger")
-      .click(function() {
-        var index = model.watchlistItems.indexOf(movie);
-        model.watchlistItems.splice(index, 1);
-        render();
-      });
+        .text("I watched it")
+        .attr("class", "btn btn-danger")
+        .click(function() {
+          var index = model.watchlistItems.indexOf(movie);
+          model.watchlistItems.splice(index, 1);
+          render();
+        });
 
     // panel heading contains the title
     var panelHeading = $("<div></div>")
-      .attr("class", "panel-heading")
-      .append(title);
-    
+        .attr("class", "panel-heading")
+        .append(title);
+
     // panel body contains the poster and button
     var panelBody = $("<div></div>")
-      .attr("class", "panel-body")
-      .append( [poster, button] );
+        .attr("class", "panel-body")
+        .append( [poster, button] );
 
     // list item is a panel, contains the panel heading and body
     var itemView = $("<li></li>")
-      .append( [panelHeading, panelBody] )
-      .attr("class", "panel panel-default");
+        .append( [panelHeading, panelBody] )
+        .attr("class", "panel panel-default");
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -156,19 +158,19 @@ function render() {
 
     // button for adding to watchlist
     var button = $("<button></button>")
-      .text("Add to Watchlist")
-      .attr("class", "btn btn-primary")
-      .click(function() {
-        model.watchlistItems.push(movie);
-        render();
-      })
-      .prop("disabled", model.watchlistItems.indexOf(movie) !== -1)
-      .attr("class", "btn btn-primary");
+        .text("Add to Watchlist")
+        .attr("class", "btn btn-primary")
+        .click(function() {
+          model.watchlistItems.push(movie);
+          render();
+        })
+        .prop("disabled", model.watchlistItems.indexOf(movie) !== -1)
+        .attr("class", "btn btn-primary");
 
     var itemView = $("<li></li>")
-      .attr("class", "list-group-item")
-      .append( [title, overview, button] );
-      
+        .attr("class", "list-group-item")
+        .append( [title, overview, button] );
+
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
